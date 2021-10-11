b4eb7db6b02f4497d8d4fd0da8ded6b113238f9d
longdv131001 <vietlong13102001@gmail.com>
2021-10-11 08:06:39 +0700

    create polygift project

---
+++ Poly_Gift/src/main/java/com/polygift/PolyGiftApplication.java
@@ -0,0 +1,13 @@
+package com.polygift;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolyGiftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolyGiftApplication.class, args);
+	}
+
+}
---
+++ Poly_Gift/src/test/java/com/polygift/PolyGiftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.polygift;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolyGiftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
