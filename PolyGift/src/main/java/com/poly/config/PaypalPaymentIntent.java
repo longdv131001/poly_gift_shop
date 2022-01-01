f93e81510a17af219eb0e9f5f53a1d2ec4426ddc
Việt Long <85155449+longdv131001@users.noreply.github.com>
2022-01-01 10:24:22 +0700

    update

--- PolyGift/src/main/java/com/poly/config/PaypalPaymentIntent.java
+++
@@ -1,5 +0,0 @@
-package com.poly.config;
-
-public enum PaypalPaymentIntent {
-    sale, authorize, order
-}
--- PolyGift/src/main/java/com/poly/config/PaypalPaymentMethod.java
+++
@@ -1,5 +0,0 @@
-package com.poly.config;
-
-public enum PaypalPaymentMethod {
-    credit_card, paypal
-}
